5a38fb684c51a8b967917fbe95cab69bb4555fe4
Sachin Rammoorthy <sachin.rammoorthy@gmail.com>
2024-04-13 15:27:26 -0400

    added gemini

--- slideCreator/config.py
+++ slideCreator/config.py
@@ -2,5 +2,8 @@
 Development configuration.
 https://flask.palletsprojects.com/en/2.2.x/config/
 """
+from dotenv import load_dotenv
+
+load_dotenv()
 
 APPLICATION_ROOT = '/'
--- slideCreator/views.py
+++ slideCreator/views.py
@@ -1,11 +1,18 @@
 import flask
 import slideCreator
 
+import google.generativeai as genai
+import os
+
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
 @slideCreator.app.route('/')
 def show_index():
     # Connect to database
     connection = slideCreator.model.get_db()
 
-    context = { "num_integrations": 1024 }
+    response = model.generate_content("New Delhi is in the country of")
+
+    context = { "some_text": response.text }
     return flask.render_template("index.html", **context)
